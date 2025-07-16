39f5aa637c34d4e84944c08bae02eb329024ed0b
Emilia Hernandez <emilia.b.hernandez@gmail.com>
2025-07-15 20:43:24 -0700

    Updated status animation

---
+++ build/js/home.js
@@ -0,0 +1,53 @@
+  const lines = [
+    "Deploying website...",
+    "Rendering 3D models on Womp...",
+    "Booting up Adobe Illustrator...",
+    "Loading Figma workspace...",
+    "Developing Hubspot modules...",
+    "Editing graphics for printing...",
+    "Designing video games...",
+    "Crafting social media content...",
+  ];
+
+  const shuffledLines = lines
+    .map(value => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value);
+
+  const textElement = document.getElementById("typewriter-text");
+  let lineIndex = 0;
+  let charIndex = 0;
+  let isDeleting = false;
+  let currentLine = shuffledLines[lineIndex];
+
+  const typeSpeed = 50;
+  const deleteSpeed = 30;
+  const delayBetweenLines = 1500;
+
+  function type() {
+    if (isDeleting) {
+      textElement.textContent = currentLine.substring(0, charIndex--);
+      if (charIndex < 0) {
+        isDeleting = false;
+        lineIndex++;
+        if (lineIndex >= shuffledLines.length) {
+          lineIndex = 0;
+          shuffledLines.sort(() => Math.random() - 0.5);
+        }
+        currentLine = shuffledLines[lineIndex];
+        setTimeout(type, 300);
+      } else {
+        setTimeout(type, deleteSpeed);
+      }
+    } else {
+      textElement.textContent = currentLine.substring(0, charIndex++);
+      if (charIndex > currentLine.length) {
+        isDeleting = true;
+        setTimeout(type, delayBetweenLines);
+      } else {
+        setTimeout(type, typeSpeed);
+      }
+    }
+  }
+
+  type();
